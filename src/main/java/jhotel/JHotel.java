71beb255555c10747477bc3d03133fec9a33c5c0
Mochamad Fahmi <mochamadfahmi.fajrin7@gmail.com>
2018-04-26 05:58:34 +0700

    Modul 8 TP

---
+++ src/main/java/jhotel/JHotel.java
@@ -0,0 +1,34 @@
+package jhotel;
+
+import java.util.GregorianCalendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+/**
+ * Ini merupakan class JHotel, terdapat fungsi main pada class ini.
+ *
+ * @author Mochamad Fahmi Fajrin
+ * @version 01/03/2018
+ */
+public class JHotel {
+
+    /**
+     * ini merupakan main method.
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(JHotel.class, args);
+
+    }
+
+    /**
+     * ini merupakan method Jhotel yang merupakan constructor.
+     *
+     * @return Nothing.
+     */
+    public JHotel() {
+
+    }
+}
